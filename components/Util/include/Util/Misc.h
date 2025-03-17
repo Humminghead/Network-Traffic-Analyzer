940acce625b5fb76f124c98eb990c9d24e2a9096
Vladimir <vladimir_pogonin@hotmail.com>
2025-03-17 19:36:24 +0300

    MakeArray util was added

--- components/Util/include/Util/Misc.h
+++ components/Util/include/Util/Misc.h
@@ -6,20 +6,33 @@ namespace Poco::Util {
 class Application;
 }
 
-namespace Nta::Util{
+namespace Nta::Util {
 namespace Misc {
 template <class Application> auto ApplicationCast(Poco::Util::Application *app) -> Application * {
     auto AppPtr = dynamic_cast<Application *>(app);
     if (!AppPtr)
         throw std::runtime_error("The application has not been derrived from Util::Application! Cast failed!");
     return AppPtr;
-}}
+}
+
+} // namespace Misc
 namespace Thread {
 /*!
  * \brief Stick the current thread to the core with specified id
  * \param Core id
  */
 void Stick2Core(const int id);
+} // namespace Thread
+
+namespace Std {
+template <typename T, size_t... Is, typename... Args>
+std::array<T, sizeof...(Is)> MakeArrayHelper(std::index_sequence<Is...>, Args &&...args) {
+    return {(static_cast<void>(Is), T{std::forward<Args>(args)...})...};
 }
 
-} // namespace Nta::Util::Misc
+template <typename T, size_t N, typename... Args> std::array<T, N> MakeArray(Args &&...args) {
+    return MakeArrayHelper<T>(std::make_index_sequence<N>{}, std::forward<Args>(args)...);
+}
+} // namespace Std
+
+} // namespace Nta::Util
