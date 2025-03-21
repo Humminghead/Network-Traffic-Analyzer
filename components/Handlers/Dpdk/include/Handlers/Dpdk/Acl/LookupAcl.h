a4beb42e426b66b4f0dc5f50dbb4fbd14fc79b7a
Vladimir <vladimir_pogonin@hotmail.com>
2025-03-21 18:55:46 +0300

    PrefetchCpuCache was moved

--- components/Handlers/Dpdk/include/Handlers/Dpdk/Acl/LookupAcl.h
+++ components/Handlers/Dpdk/include/Handlers/Dpdk/Acl/LookupAcl.h
@@ -172,7 +172,7 @@ class RteAclContext {
 class RteLookupAcl {
   public:
     using Result = std::pair<bool, std::vector<uint32_t>>;
-    using PacketPointers = std::vector<const uint8_t *>;
+    using PacketPointers = std::array<const uint8_t *,64>;
 
     /*!
      * \brief Classify
@@ -181,9 +181,7 @@ class RteLookupAcl {
      * \param categories
      * \return
      */
-    Result Classify(const RteAclContext &ctx, PacketPointers &packets, const uint32_t categories = 1);
+    Result Classify(const RteAclContext &ctx, PacketPointers &packets, const size_t packetsSize, const uint32_t categories = 1);
 };
-
-auto PrefetchCpuCache(const std::vector<rte_mbuf *> &rxPkts, const size_t prefetchCount) -> void;
 
 } // namespace Nta::Network
