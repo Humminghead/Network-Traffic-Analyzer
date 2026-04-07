61bb415052d622cf4ac57987d5c6ad485362f79a
Vladimir <vladimir_pogonin@hotmail.com>
2026-04-07 20:15:41 +0300

    projec conan file was changed
    dpdk downgraded, some options was added

--- conanfile.py
+++ conanfile.py
@@ -9,15 +9,14 @@ class NetworkAnalyzer(ConanFile):
     generators = "CMakeDeps"
 
     def requirements(self):
-        self.requires("dpdk/26.03")
-        self.requires("pcapplusplus/25.05")
+        self.requires("dpdk/21.11.9")
+        self.requires("pcapplusplus/25.05@")
         self.requires("thrift_pfr_serializer/1.0.5")
         self.requires("nlohmann_json/3.11.3")
-        self.requires("poco/1.13.3")
+        self.requires("poco/1.15.1")
 
     def generate(self):        
-        tc = CMakeToolchain(self)
-        tc.cache_variables["PCAPPP_USE_DPDK"] = "ON"
+        tc = CMakeToolchain(self)        
         # If DPDK is not in a standard path, you can also set its root
         # tc.cache_variables["DPDK_ROOT"] = self.dependencies["dpdk"].package_folder
         tc.user_presets_path = "build/ConanPresets.json"
@@ -30,3 +29,28 @@ class NetworkAnalyzer(ConanFile):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
+
+    def configure(self):
+        self.options["pcapplusplus"].with_dpdk = True
+        # Disable unnecessary Poco components to speed up build and reduce size
+        self.options["poco"].enable_net = False
+        # self.options["poco"].enable_data = False
+        self.options["poco"].enable_data_mysql = False
+        self.options["poco"].enable_data_postgresql = False
+        self.options["poco"].enable_netssl = False          # Keep if you need SSL        
+        self.options["poco"].enable_util = True
+        self.options["poco"].enable_json = False
+        self.options["poco"].enable_xml = False
+        self.options["poco"].enable_encodings = True
+        self.options["poco"].enable_zip = False
+        self.options["poco"].enable_sevenzip = False
+        self.options["poco"].enable_crypto = False
+        self.options["poco"].enable_file2page = False
+        self.options["poco"].enable_jwt = False
+        self.options["poco"].enable_mongodb = False
+        self.options["poco"].enable_pagecompiler = False
+        self.options["poco"].enable_pdf = False
+        self.options["poco"].enable_redis = False
+        self.options["poco"].enable_tasks = False
+        self.options["poco"].enable_timer = False
+        self.options["boost"].without_cobalt = True
