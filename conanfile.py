af86809aec07e82714ad44cbe532ea9ae741a3e6
Vladimir <vladimir_pogonin@hotmail.com>
2026-04-08 19:12:40 +0300

    poco opts were deleted

--- conanfile.py
+++ conanfile.py
@@ -11,7 +11,7 @@ class NetworkAnalyzer(ConanFile):
     def requirements(self):
         self.requires("dpdk/21.11.9")
         self.requires("pcapplusplus/25.05@")
-        self.requires("thrift_pfr_serializer/1.0.5")
+        self.requires("thrift_pfr_serializer/1.0.6")
         self.requires("nlohmann_json/3.11.3")
         self.requires("poco/1.15.1")
 
@@ -31,26 +31,5 @@ class NetworkAnalyzer(ConanFile):
         cmake.build()
 
     def configure(self):
-        self.options["pcapplusplus"].with_dpdk = True
-        # Disable unnecessary Poco components to speed up build and reduce size
-        self.options["poco"].enable_net = False
-        # self.options["poco"].enable_data = False
-        self.options["poco"].enable_data_mysql = False
-        self.options["poco"].enable_data_postgresql = False
-        self.options["poco"].enable_netssl = False          # Keep if you need SSL        
-        self.options["poco"].enable_util = True
-        self.options["poco"].enable_json = False
-        self.options["poco"].enable_xml = False
-        self.options["poco"].enable_encodings = True
-        self.options["poco"].enable_zip = False
-        self.options["poco"].enable_sevenzip = False
-        self.options["poco"].enable_crypto = False
-        self.options["poco"].enable_file2page = False
-        self.options["poco"].enable_jwt = False
-        self.options["poco"].enable_mongodb = False
-        self.options["poco"].enable_pagecompiler = False
-        self.options["poco"].enable_pdf = False
-        self.options["poco"].enable_redis = False
-        self.options["poco"].enable_tasks = False
-        self.options["poco"].enable_timer = False
+        self.options["pcapplusplus"].with_dpdk = True        
         self.options["boost"].without_cobalt = True
