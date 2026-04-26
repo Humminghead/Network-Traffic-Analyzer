7d22dc5d08e5d2811d630f4721d8b02baa6a36ab
Vladimir <vladimir_pogonin@hotmail.com>
2026-04-26 16:14:13 +0300

    "no-shconf" option was added

--- components/Handlers/Dpdk/include/Handlers/Dpdk/JsonObjectDpdk.h
+++ components/Handlers/Dpdk/include/Handlers/Dpdk/JsonObjectDpdk.h
@@ -115,6 +115,7 @@ struct DpdkObject : HandlerObject {
     bool m_PromiscuousMode{false};
     bool m_NoPci{false};
     bool m_InMemory{false};
+    bool m_NoShconf{false};
     std::vector<Worker> workers{};
 
     [[maybe_unused]] static auto ToJson(const DpdkObject &p) -> nlohmann::json {
@@ -122,6 +123,7 @@ struct DpdkObject : HandlerObject {
         return {
              {"no-pci", p.m_NoPci},
              {"in-memory", p.m_InMemory},
+             {"no-shconf", p.m_NoShconf},
              {"eal_mbuf_size", p.m_BufPoolSizePerDevice},
              {"eal_main_lcore", p.m_MainLcore},
              {"eal_memory_channels", p.m_NumOfMemoryChannels},
@@ -136,6 +138,7 @@ struct DpdkObject : HandlerObject {
     [[maybe_unused]] static void FromJson(const nlohmann::json &j, DpdkObject &p) {
         j.at("no-pci").get_to(p.m_NoPci);
         j.at("in-memory").get_to(p.m_InMemory);
+        j.at("no-shconf").get_to(p.m_NoShconf);
         j.at("eal_mbuf_size").get_to(p.m_BufPoolSizePerDevice);
         j.at("eal_memory_channels").get_to(p.m_NumOfMemoryChannels);
         Util::Json::GetTo(j, "eal_cmd_line_arguments", p.m_EalCmdLine);
