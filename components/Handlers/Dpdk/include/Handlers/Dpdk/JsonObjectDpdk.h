63a6520e45b74ca5126b4f4528f69a11c46fd35a
Vladimir <vladimir_pogonin@hotmail.com>
2026-04-26 16:18:29 +0300

    "no-telemetry" option was added

--- components/Handlers/Dpdk/include/Handlers/Dpdk/JsonObjectDpdk.h
+++ components/Handlers/Dpdk/include/Handlers/Dpdk/JsonObjectDpdk.h
@@ -117,6 +117,7 @@ struct DpdkObject : HandlerObject {
     bool m_InMemory{false};
     bool m_NoShconf{false};
     bool m_NoHuge{false};
+    bool m_NoTelemetry{false};
     std::vector<Worker> workers{};
 
     [[maybe_unused]] static auto ToJson(const DpdkObject &p) -> nlohmann::json {
@@ -126,6 +127,7 @@ struct DpdkObject : HandlerObject {
              {"in-memory", p.m_InMemory},
              {"no-shconf", p.m_NoShconf},
              {"no-huge", p.m_NoHuge},
+             {"no-telemetry", p.m_NoTelemetry},
              {"eal_mbuf_size", p.m_BufPoolSizePerDevice},
              {"eal_main_lcore", p.m_MainLcore},
              {"eal_memory_channels", p.m_NumOfMemoryChannels},
@@ -142,6 +144,7 @@ struct DpdkObject : HandlerObject {
         j.at("in-memory").get_to(p.m_InMemory);
         j.at("no-shconf").get_to(p.m_NoShconf);
         j.at("no-huge").get_to(p.m_NoHuge);
+        j.at("no-huge").get_to(p.m_NoTelemetry);
         j.at("eal_mbuf_size").get_to(p.m_BufPoolSizePerDevice);
         j.at("eal_memory_channels").get_to(p.m_NumOfMemoryChannels);
         Util::Json::GetTo(j, "eal_cmd_line_arguments", p.m_EalCmdLine);
