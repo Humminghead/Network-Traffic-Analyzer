b5f114aa3aedcc53d7a0aa5e0791752e5775ae1a
Vladimir <vladimir_pogonin@hotmail.com>
2026-04-28 22:40:59 +0300

    util/json: 'GetTo' util with predicate

--- components/Util/include/Util/Json.h
+++ components/Util/include/Util/Json.h
@@ -17,4 +17,9 @@ template <typename ValueType>
 static inline void GetTo(const nlohmann::json &j, const std::string &name, ValueType &value) noexcept {
     value = j.contains(name) ? j.at(name).get_to(value) : value;
 }
+
+template <typename ValueType, typename Predicate>
+static inline void GetTo(const nlohmann::json &j, const std::string &name, ValueType &value, Predicate pred) noexcept {
+    value = j.contains(name) ? pred(j, name, value) : value;
+}
 } // namespace Nta::Util::Json
