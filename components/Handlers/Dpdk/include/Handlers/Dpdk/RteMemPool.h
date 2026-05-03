30f6419cff2be0a2ba01e6abd46eefec78fe2e51
Vladimir <vladimir_pogonin@hotmail.com>
2026-05-03 19:11:39 +0300

    handlers/dpdk: free function was added in RteMemPool

--- components/Handlers/Dpdk/include/Handlers/Dpdk/RteMemPool.h
+++ components/Handlers/Dpdk/include/Handlers/Dpdk/RteMemPool.h
@@ -1,26 +1,44 @@
 #pragma once
 
-#include <rte_mbuf.h>
 #include <rte_errno.h>
+#include <rte_mbuf.h>
 #include <stdexcept>
 
 class RteMemPool {
-    struct rte_mempool *mbuf_mp{nullptr};
+    struct rte_mempool *m_mBufPtr{nullptr};
 
   public:
+    /*!
+     * \brief RteMemPool
+     * \param The name of the mbuf pool.
+     * \param The number of elements in the mbuf pool.
+     * \param Size of the per-core object cache. See rte_mempool_create() for details.
+     * \param     Size of application private are between the rte_mbuf structure
+    and the data buffer. This value must be aligned to RTE_MBUF_PRIV_ALIGN.
+     * \param Size of data buffer in each mbuf, including RTE_PKTMBUF_HEADROOM
+     * \param The socket identifier where the memory should be allocated.
+     */
     RteMemPool(
         const char *name,
-        unsigned int total_mbuf_num,
-        unsigned int mbuf_cache_size,
-        uint16_t priv_size,
-        uint16_t data_room_size,
-        int socket_id) {
+        unsigned int totalMbufNum,
+        unsigned int mbufCacheSize,
+        uint16_t privSize,
+        uint16_t dataRoomSize,
+        int socketId) {
 
-        mbuf_mp = rte_pktmbuf_pool_create(name, total_mbuf_num, mbuf_cache_size, priv_size, data_room_size, socket_id);
+        m_mBufPtr = rte_pktmbuf_pool_create(name, totalMbufNum, mbufCacheSize, privSize, dataRoomSize, socketId);
 
-        if (mbuf_mp == nullptr)
-            throw std::runtime_error("Error: can't init mbuf pool: "+ std::string{rte_strerror(rte_errno)});        
+        if (m_mBufPtr == nullptr)
+            throw std::runtime_error("Error: can't init mbuf pool: " + std::string{rte_strerror(rte_errno)});
     }
 
-    auto GetRteMemPoolPtr() const { return mbuf_mp; }
+    /*!
+     * \brief Get a mbuf pool ptr.
+     */
+    auto GetRteMemPoolPtr() const { return m_mBufPtr; }
+
+    /*!
+     * \brief Free a mempool
+     */
+    void Free() noexcept { rte_mempool_free(m_mBufPtr); }
 };
