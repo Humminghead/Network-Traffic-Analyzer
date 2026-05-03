b4a19134375586da0ac6357515687017984e916c
Vladimir <vladimir_pogonin@hotmail.com>
2026-05-03 10:50:27 +0300

    comp/device: library iface was enhanced
    New fictions was added

--- components/Device/include/Device.h
+++ components/Device/include/Device.h
@@ -1,5 +1,14 @@
 #pragma once
 
+#include <cstdint>
+#include <vector>
+
+///\todo Move it in separate header
+struct rte_mbuf;
+namespace Nta::Network {
+using MbufArray = std::vector<rte_mbuf *>;
+}
+
 namespace Nta::Network::Device {
 class AbstractDevice {
   protected:
@@ -8,9 +17,19 @@ class AbstractDevice {
   public:
     virtual ~AbstractDevice() = default;
 
+    AbstractDevice &operator=(AbstractDevice &&) = default;
+    AbstractDevice(AbstractDevice &&) = default;
+
+    AbstractDevice &operator=(const AbstractDevice &) = delete;
+    AbstractDevice(const AbstractDevice &) = delete;
+
+    virtual void SetupRxQueue(const uint16_t queueId) = 0;
+    virtual void SetupTxQueue(const uint16_t queueId) = 0;
     virtual void Open() = 0;
     virtual void Close() = 0;
     virtual bool IsOpen() const = 0;
+    virtual uint16_t SendPackets(const uint16_t queueId, MbufArray &bufArray, const uint16_t nbPkts) = 0;
+    virtual uint16_t RecivePackets(const uint16_t queueId, MbufArray &m_BufArray) = 0;
 };
 
 template <typename... Product> class AbstractDeviceFactory : public Product... {
