cd4c6eb7c3323490cf7fabe075e9098d615c7f18
Vladimir <vladimir_pogonin@hotmail.com>
2026-05-03 18:03:55 +0300

    handlers/dpdk: main dpdk handler's code was up

--- components/Handlers/Dpdk/include/Handlers/Dpdk/HandlerDpdk.h
+++ components/Handlers/Dpdk/include/Handlers/Dpdk/HandlerDpdk.h
@@ -3,9 +3,8 @@
 #include <Handlers/Common/HandlerIface.h>
 #include <memory>
 
-namespace pcpp
-{
-class DpdkWorkerThread;
+namespace Nta::Network {
+class DpdkWorker;
 }
 
 namespace Nta::Json::Objects {
@@ -13,6 +12,8 @@ struct DpdkObject;
 }
 
 namespace Nta::Network {
+
+using DpdkWorkerPtr = std::unique_ptr<DpdkWorker>;
 
 class HandlerDpdk : public HandlerAbstract {
 public:
@@ -60,7 +61,7 @@ public:
     auto GetIfaceType() const -> const HandlerIfaces override { return HandlerIfaces::Pcap; }
 
 private:
-    bool StartDpdkWorkerThreads(const uint32_t coreMask, std::vector<pcpp::DpdkWorkerThread*>& workerThreadsVec);
+    bool StartDpdkWorkerThreads(std::vector<DpdkWorkerPtr>& workerThreadsVec);
     void StopDpdkWorkerThreads();
 
     struct Impl;
