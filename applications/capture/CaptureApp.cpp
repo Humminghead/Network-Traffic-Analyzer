8e07558bd3ecc8baacce4e5b337f1932afea79ce
Vladimir <vladimir_pogonin@hotmail.com>
2026-05-08 20:38:41 +0300

    app/capture: POSIX signals processing was added

--- applications/capture/CaptureApp.cpp
+++ applications/capture/CaptureApp.cpp
@@ -2,11 +2,97 @@
 #include "Handlers/Common/HandlerIface.h"
 #include "Util/Filesystem.h"
 #include "Util/Misc.h"
+#include <csignal>
+#include <functional>
 #include <iostream>
+#include <print>
 
 namespace Nta::Network {
 
+namespace SigHandler {
+
+class Storage {
+  private:
+    using func_t = std::function<int(int)>;
+
+    template <typename F> using is_function = std::enable_if<std::is_function_v<std::remove_reference_t<F>>>;
+
+    std::vector<func_t> m_StopFunctions{};
+
+  public:
+    constexpr auto operator()(int signal) -> void {
+        for (auto &f : m_StopFunctions) {
+            f(signal);
+        }
+    };
+
+    template <typename... F, is_function<F>...> constexpr Storage(F &&...f) {
+        (m_StopFunctions.push_back(func_t{std::move(f)}), ...);
+    }
+
+    template <typename... F, is_function<F>...> constexpr auto Add(F &&...f) {
+        return (m_StopFunctions.push_back(func_t{std::move(f)}), ...);
+    }
+};
+
+namespace {
+thread_local static std::unique_ptr<Storage> storage;
+}
+
+template <class T> static constexpr auto Add(T &&f) {
+    if (storage == nullptr) {
+        storage = std::make_unique<Storage>(std::move(f));
+        return;
+    }
+    storage->Add(std::move(f));
+}
+
+static auto HandlerFunc(int s) {
+    if (!storage)
+        return;
+
+    (*storage)(s);
+}
+
+} // namespace SigHandler
+
+auto stopWarn = [](int sig) {
+    std::println("{}: stopped because signal {} has been catched!", "APP", sig);
+    std::fflush(stdout);
+};
+
+CaptureApp::CaptureApp()
+    : ServerApplication(),                                                 //
+      m_Configure{std::make_unique<ConfigureSubsystem>()},                 //
+      m_Capture{std::make_unique<CaptureSubsystem>(m_Configure.get())},    //
+      m_Decode{std::make_unique<DecodeSubsystem>(m_Configure.get())},      //
+      m_Transport{std::make_unique<TransportSubsystem>(m_Configure.get())} //
+{
+    SigHandler::Add([this](int signal) {
+        stopWarn(signal);
+        return this->Stop();
+    });
+}
+
+CaptureApp::~CaptureApp() {
+    Poco::Util::ServerApplication::uninitialize();
+    this->subsystems().clear();
+
+    // It's needed because class Poco::Util::SubsystemSubsystem derrived from Poco::RefCountedObject (AutoPtr)
+    m_Configure.release();
+    m_Capture.release();
+    m_Decode.release();
+    m_Transport.release();
+}
+
 int CaptureApp::main(const std::vector<std::string> &args) {
+    std::signal(SIGINT, SigHandler::HandlerFunc);
+    std::signal(SIGTERM, SigHandler::HandlerFunc);
+    std::signal(SIGQUIT, SigHandler::HandlerFunc);
+    std::signal(SIGABRT, SigHandler::HandlerFunc);
+    std::signal(SIGHUP, SigHandler::HandlerFunc);
+    std::signal(SIGKILL, SigHandler::HandlerFunc);
+
     if (m_HelpRequested || m_ConfigPath.empty()) {
         DisplayHelp();
         return 0;
@@ -58,12 +144,24 @@ int CaptureApp::Run() {
         m_Capture->GetHandler()->Open();
         m_Capture->GetHandler()->Loop();
     } catch (const std::exception &e) {
+        Stop();
         ///\todo LOG
         std::cerr << e.what() << std::endl;
         return Application::EXIT_SOFTWARE;
     }
-    m_Capture->GetHandler()->Close();
 
+    Stop();
+
+    return Application::EXIT_OK;
+}
+
+int CaptureApp::Stop() {
+    try {
+        m_Capture->GetHandler()->Close();
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+        return Application::EXIT_SOFTWARE;
+    }
     return Application::EXIT_OK;
 }
 
@@ -79,25 +177,6 @@ void CaptureApp::initialize(Application &self) {
     Poco::Util::Application::initialize(self);
 
     m_AppCore = m_Configure->GetAppCore<decltype(m_AppCore)>(-1);
-}
-
-CaptureApp::CaptureApp()
-    : ServerApplication(),                                                 //
-      m_Configure{std::make_unique<ConfigureSubsystem>()},                 //
-      m_Capture{std::make_unique<CaptureSubsystem>(m_Configure.get())},    //
-      m_Decode{std::make_unique<DecodeSubsystem>(m_Configure.get())},      //
-      m_Transport{std::make_unique<TransportSubsystem>(m_Configure.get())} //
-{}
-
-CaptureApp::~CaptureApp() {
-    Poco::Util::ServerApplication::uninitialize();
-    this->subsystems().clear();
-
-    // It's needed because class Poco::Util::SubsystemSubsystem derrived from Poco::RefCountedObject (AutoPtr)
-    m_Configure.release();
-    m_Capture.release();
-    m_Decode.release();
-    m_Transport.release();
 }
 
 auto CaptureApp::GetConfigPath() const noexcept -> std::filesystem::path {
