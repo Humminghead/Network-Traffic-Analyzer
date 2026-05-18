3a31f005011e553ff958b736cf53c657e06dc0cd
Vladimir <vladimir_pogonin@hotmail.com>
2026-05-18 18:41:46 +0400

    handlers/dpdk: packetsSize variable was renamed

--- components/Handlers/Dpdk/src/Acl/LookupAcl.cpp
+++ components/Handlers/Dpdk/src/Acl/LookupAcl.cpp
@@ -26,10 +26,10 @@ auto Nta::Network::RteAclContext::Build() -> void {
 Nta::Network::RteLookupAcl::Result Nta::Network::RteLookupAcl::Classify(
     const RteAclContext &ctx,
     PacketPointers &packets,
-    const size_t packetsSize,
+    const size_t packetsNum,
     const uint32_t categories) {
-    std::vector<uint32_t> searchResult(packetsSize, uint32_t{0});
+    std::vector<uint32_t> searchResult(packetsNum, uint32_t{0});
     return {
-        0 == rte_acl_classify(ctx.RawPointer(), packets.data(), searchResult.data(), packetsSize, categories),
+        0 == rte_acl_classify(ctx.RawPointer(), packets.data(), searchResult.data(), packetsNum, categories),
         searchResult};
 }
